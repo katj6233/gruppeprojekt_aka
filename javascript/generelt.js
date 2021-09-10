05c0cff2bed23938f0be3533d95e056140a6aa86
“ande397c” <ande397c@stud.kea.dk>
2021-09-10 10:12:10 +0200

    burger + @media

--- javascript/generelt.js
+++ javascript/generelt.js
@@ -1,1 +1,34 @@
-console.log("hej");
+window.addEventListener("DOMContentLoaded", start);
+
+const links = document.querySelector(".nav_links");
+
+const burger = document.querySelector(".burger");
+
+const li = document.querySelectorAll("li");
+
+function start() {
+  burger.addEventListener("click", toggle);
+  burger.addEventListener("mouseover", over);
+  burger.addEventListener("mouseout", done);
+}
+
+function toggle() {
+  console.log(burger.classList.contains("toggle"));
+
+  if (burger.classList.contains("toggle")) {
+    burger.addEventListener("mouseover", over);
+  } else {
+    burger.removeEventListener("mouseover", over);
+  }
+  links.classList.toggle("nav_toggle");
+  burger.classList.toggle("toggle");
+}
+
+function over() {
+  console.log("over");
+  burger.classList.add("hover");
+}
+function done() {
+  console.log("done");
+  burger.classList.remove("hover");
+}
