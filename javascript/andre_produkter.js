f55e18994e39c2e7519c7284d6509bc28c3df845
“ande397c” <ande397c@stud.kea.dk>
2021-09-14 09:40:35 +0200

    bredcrumbs produktDetaljer + nyt script

---
+++ javascript/andre_produkter.js
@@ -0,0 +1,39 @@
+let alleUre;
+let container;
+let temp;
+
+const url = "https://kea2021-9221.restdb.io/rest/armbaandsure";
+
+const options = {
+  headers: {
+    "x-apikey": "6139fbde43cedb6d1f97eeee",
+  },
+};
+
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  container = document.querySelector("#lignende_produkter");
+  temp = document.querySelector("template");
+  // console.log(temp);
+  hentdata();
+}
+
+async function hentdata() {
+  const result = await fetch(url, options);
+  alleUre = await result.json();
+  console.log(alleUre);
+  visUre();
+}
+
+function visUre() {
+  container.textContent = "";
+  alleUre.forEach((ur) => {
+    let klon = temp.cloneNode(true).content;
+    klon.querySelector("h2").textContent = ur.Navn;
+    klon.querySelector(".farve").textContent = ur.Farve;
+    klon.querySelector(".pris").textContent = ur.Pris + ",-";
+    klon.querySelector("img").src = "images/" + ur.Billede + ".jpg";
+    container.appendChild(klon);
+  });
+}
